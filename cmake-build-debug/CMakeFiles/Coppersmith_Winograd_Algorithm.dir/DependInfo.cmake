5731d101c861385d464dea49ee3e03aa77840113
XG-PRO <knperros@csd.auth.gr>
2021-03-06 13:48:58 +0200

    fixed cmake

---
+++ cmake-build-debug/CMakeFiles/Coppersmith_Winograd_Algorithm.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/XG-PRO/Desktop/C PROJECTS/Coppersmith-Winograd-Algorithm/Determinant v2.c" "C:/Users/XG-PRO/Desktop/C PROJECTS/Coppersmith-Winograd-Algorithm/cmake-build-debug/CMakeFiles/Coppersmith_Winograd_Algorithm.dir/Determinant_v2.c.obj"
+  "C:/Users/XG-PRO/Desktop/C PROJECTS/Coppersmith-Winograd-Algorithm/main.c" "C:/Users/XG-PRO/Desktop/C PROJECTS/Coppersmith-Winograd-Algorithm/cmake-build-debug/CMakeFiles/Coppersmith_Winograd_Algorithm.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../."
+  "../.idea"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Coppersmith_Winograd_Algorithm.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Coppersmith_Winograd_Algorithm.dir/Determinant_v2.c.obj"
+  "CMakeFiles/Coppersmith_Winograd_Algorithm.dir/main.c.obj"
+  "Coppersmith_Winograd_Algorithm.exe"
+  "Coppersmith_Winograd_Algorithm.exe.manifest"
+  "Coppersmith_Winograd_Algorithm.pdb"
+  "libCoppersmith_Winograd_Algorithm.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Coppersmith_Winograd_Algorithm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
