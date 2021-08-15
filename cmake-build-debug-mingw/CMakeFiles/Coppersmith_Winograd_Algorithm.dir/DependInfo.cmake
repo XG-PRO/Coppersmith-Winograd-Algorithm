10c3eaa42a5c0f9d8ef7ae4922ecc810b70c8b93
DimYfantidis <axaiosyfan@gmail.com>
2021-08-15 12:22:51 +0300

    Problem with PA = LU decomposition.

--- cmake-build-debug-mingw/CMakeFiles/Coppersmith_Winograd_Algorithm.dir/DependInfo.cmake
+++ cmake-build-debug-mingw/CMakeFiles/Coppersmith_Winograd_Algorithm.dir/DependInfo.cmake
@@ -1,3 +1,7 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "CXX"
@@ -12,6 +16,10 @@ set(CMAKE_CXX_COMPILER_ID "GNU")
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "../."
   "../.idea"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
--- complex.h
+++ complex.h
@@ -5,175 +5,171 @@
 
 namespace algebra
 {
-	class complex 
-	{
-	protected:
-		double a;	// Real part
-		double b;	// Imaginary part
+    class complex {
+    protected:
+        double a;	// Real part
+        double b;	// Imaginary part
 
-	public:	// Constructors
-		complex();
-		complex(double, double);
-		complex(const complex&) = default;
-		~complex() = default;
+    public:	// Constructors
+        complex();
+        complex(double);
+        complex(double, double);
+        complex(const complex&) = default;
+        ~complex() = default;
 
-	public:	// Methods
-		void set_real(double a) { this->a = a; }
-		void set_imaginary(double b) { this->b = b; }
+    public:	// Methods
+        void set_real(double a) { this->a = a; }
+        void set_imaginary(double b) { this->b = b; }
 
-		double real() const { return a; }
-		double imaginary() const { return b; }
+        double real() const { return a; }
+        double imaginary() const { return b; }
 
-	public:	// Operators
-		complex& operator += (const complex&);
-		complex& operator -= (const complex&);
-		complex& operator /= (const complex&);
-		complex& operator *= (const complex&);
-	};
+    public:	// Operators
+        complex& operator += (const complex&);
+        complex& operator -= (const complex&);
+        complex& operator /= (const complex&);
+        complex& operator *= (const complex&);
+    };
 
-	// Operators
-	complex operator + (const complex&, const complex&);
-	complex operator - (const complex&, const complex&);
-	complex operator * (const complex&, const complex&);
-	complex operator / (const complex&, const complex&);
-	std::istream& operator >> (std::istream&, complex&);
-	std::ostream& operator << (std::ostream&, const complex&);
-
-
-	// --- BLUEPRINTS ---
-
-	// Empty Constructor
-	complex::complex() {
-		a = 0;
-		b = 0;
-	}
-	// Explicit Constructors
-	complex::complex(double a, double b) {
-	    this->a = a;
-	    this->b = b;
-	}
+    // Operators
+    complex operator + (const complex&, const complex&);
+    complex operator - (const complex&, const complex&);
+    complex operator * (const complex&, const complex&);
+    complex operator / (const complex&, const complex&);
+    std::istream& operator >> (std::istream&, complex&);
+    std::ostream& operator << (std::ostream&, const complex&);
 
 
-	// --- OPERATORS ---
+    // --- BLUEPRINTS ---
+
+    // Empty Constructor
+    complex::complex() {
+        a = 0;
+        b = 0;
+    }
+    // Converting Constructors
+    complex::complex(double a, double b) {
+        this->a = a;
+        this->b = b;
+    }
+    complex::complex(double k) {
+        this->a = k;
+        this->b = k;
+    }
 
 
-	// Plus-equals operator
-	complex& complex::operator += (const complex& arg) {
-		a += arg.a;
-		b += arg.b;
-		return *this;
-	}
+    // --- OPERATORS ---
 
-	// Minus-equals operator
-	complex& complex::operator -= (const complex& arg) {
-		a -= arg.a;
-		b -= arg.b;
-		return *this;
-	}
 
-	// Times-equals operator
-	complex& complex::operator *= (const complex& arg) 
-	{
-		double c = arg.a;
-		double d = arg.b;
+    // Plus-equals operator
+    complex& complex::operator += (const complex& arg) {
+        a += arg.a;
+        b += arg.b;
+        return *this;
+    }
 
-		a = a * c - b * d;
-		b = a * d + b * c;
+    // Minus-equals operator
+    complex& complex::operator -= (const complex& arg) {
+        a -= arg.a;
+        b -= arg.b;
+        return *this;
+    }
 
-		return *this;
-	}
+    // Times-equals operator
+    complex& complex::operator *= (const complex& arg) {
+        double c = arg.a;
+        double d = arg.b;
 
-	// Divided-equals operator
-	complex& complex::operator /= (const complex& arg) 
-	{
-		double c = arg.a;
-		double d = arg.b;
-		double a_prev = this->real();
+        a = a * c - b * d;
+        b = a * d + b * c;
 
-		a = (a * c + b * d) / (c * c + d * d);
-		b = (b * c - a_prev * d) / (c * c + d * d);
+        return *this;
+    }
 
-		return *this;
-	}
+    // Divided-equals operator
+    complex& complex::operator /= (const complex& arg) {
+        double c = arg.a;
+        double d = arg.b;
+        double a_prev = this->real();
 
-	// Addition operator
-	complex operator + (const complex& one, const complex& two) 
-	{
-		complex sum;
-		sum.set_real(one.real() + two.real());
-		sum.set_imaginary(one.imaginary() + two.imaginary());
-		return sum;
-	}
+        a = (a * c + b * d) / (c * c + d * d);
+        b = (b * c - a_prev * d) / (c * c + d * d);
 
-	// Subtraction operator
-	complex operator - (const complex& one, const complex& two) 
-	{
-		complex diff;
-		diff.set_real(one.real() - two.real());
-		diff.set_imaginary(one.imaginary() - two.imaginary());
-		return diff;
-	}
+        return *this;
+    }
 
-	// Multiplication operator
-	complex operator * (const complex& one, const complex& two) 
-	{
-		complex prod;
+    // Addition operator
+    complex operator + (const complex& one, const complex& two) {
+        complex sum;
+        sum.set_real(one.real() + two.real());
+        sum.set_imaginary(one.imaginary() + two.imaginary());
+        return sum;
+    }
 
-		double a = one.real();
-		double b = one.imaginary();
-		double c = two.real();
-		double d = two.imaginary();
+    // Subtraction operator
+    complex operator - (const complex& one, const complex& two) {
+        complex diff;
+        diff.set_real(one.real() - two.real());
+        diff.set_imaginary(one.imaginary() - two.imaginary());
+        return diff;
+    }
 
-		prod.set_real(a * c - b * d);
-		prod.set_imaginary(a * d + b * c);
+    // Multiplication operator
+    complex operator * (const complex& one, const complex& two) {
+        complex prod;
 
-		return prod;
-	}
+        double a = one.real();
+        double b = one.imaginary();
+        double c = two.real();
+        double d = two.imaginary();
 
-	// Division operator
-	complex operator / (const complex& one, const complex& two) 
-	{
-		complex div;
+        prod.set_real(a * c - b * d);
+        prod.set_imaginary(a * d + b * c);
 
-		double a = one.real();
-		double b = one.imaginary();
-		double c = two.real();
-		double d = two.imaginary();
+        return prod;
+    }
 
-		div.set_real((a * c + b * d) / (c * c + d * d));
-		div.set_imaginary((b * c - a * d) / (c * c + d * d));
+    // Division operator
+    complex operator / (const complex& one, const complex& two) {
+        complex div;
 
-		return div;
-	}
+        double a = one.real();
+        double b = one.imaginary();
+        double c = two.real();
+        double d = two.imaginary();
 
-	// Input stream operator
-	std::istream& operator >> (std::istream& is, complex& arg) 
-	{
-		double a;
-		double b;
+        div.set_real((a * c + b * d) / (c * c + d * d));
+        div.set_imaginary((b * c - a * d) / (c * c + d * d));
 
-		is >> a >> b;
-		arg.set_real(a);
-		arg.set_imaginary(b);
+        return div;
+    }
 
-		return is;
-	}
+    // Input stream operator
+    std::istream& operator >> (std::istream& is, complex& arg) {
+        double a;
+        double b;
 
-	// Output stream operator
-	std::ostream& operator << (std::ostream& os, const complex& arg) 
-	{
-		double b_abs = abs(arg.imaginary());
+        is >> a >> b;
+        arg.set_real(a);
+        arg.set_imaginary(b);
 
-		os << arg.real();
-		if (b_abs != 0) {
-			os << (arg.imaginary() > 0 ? " + " : " - ");
-			if (b_abs != 1) {
-				os << b_abs;
-			}
-			os << "i";
-		}
-		return os;
-	}
+        return is;
+    }
+
+    // Output stream operator
+    std::ostream& operator << (std::ostream& os, const complex& arg) {
+        double b_abs = abs(arg.imaginary());
+
+        os << arg.real();
+        if (b_abs != 0) {
+            os << (arg.imaginary() > 0 ? " + " : " - ");
+            if (b_abs != 1) {
+                os << b_abs;
+            }
+            os << "i";
+        }
+        return os;
+    }
 }
 
 #endif // COMPLEX_H
